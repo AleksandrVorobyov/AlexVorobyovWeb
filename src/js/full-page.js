2cb33c1a37c36018549f4a56bd1fa0212988d610
Alex <sasha89253215539@yandex.ru>
2020-12-10 15:03:33 +0300

    добавил новыt картинки

--- src/js/full-page.js
+++ src/js/full-page.js
@@ -2,22 +2,25 @@ const fullPage = document.querySelector('.full-page');
 const allCorouselImg = document.querySelectorAll('.carousel-project .owl-carousel-item img');
 const cardPostWrap = document.querySelector('.card-post-wrap');
 const fullPageWrap = document.querySelector('.full-page-wrap');
+const scrollY = document.body.style.top;
 
 allCorouselImg.forEach(function(item) {
     item.addEventListener('click', function() {
-        document.querySelector('body').style.overflow='hidden';
+        body.style.overflow='hidden';
         fullPage.classList.add('full-page--active');
+
     })
 })
 
 const fullPageExit = document.querySelector('.full-page__exit');
 
 fullPageExit.addEventListener('click', function () {
-    document.querySelector('body').style.overflow='scroll';
+    body.style.overflow='scroll';
     fullPage.classList.remove('full-page--active');
 })
 
 fullPage.onclick = function(){
     fullPage.classList.remove('full-page--active');
-    document.querySelector('body').style.overflow='scroll';
+    body.style.overflow='scroll';
 };
+
