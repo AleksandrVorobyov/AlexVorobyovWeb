fa16e5752999888d40230dcc6b327729eb169326
Alex <sasha89253215539@yandex.ru>
2021-01-07 00:06:10 +0300

    new btn and fix animate

---
+++ docs/js/animate.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function($) {new WOW().init();});
+<script src="./js/wow.min.js"></script>
+jQuery(document).ready(function($) {$('.wow').attr('data-wow-duration', '1.5s' );});
