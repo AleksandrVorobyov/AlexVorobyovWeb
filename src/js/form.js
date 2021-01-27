5b37a8b70bc2db3ecc78483934facdfb9919a39e
Alex <sasha89253215539@yandex.ru>
2021-01-27 20:06:02 +0300

    fix js

--- src/js/form.js
+++ src/js/form.js
@@ -4,14 +4,17 @@ const formtheme = document.getElementById('theme-form');
 const formtext = document.getElementById('text-form');
 const formBtn = document.getElementById('button-form');
 
-formContact.addEventListener('submit', function(e) {
-  if (validateEmail(formemail.value) && formtheme.value !== '' && formtext.value !== '') {
-    
-  } else {
-    e.preventDefault();
-    alert('Ошибка ввода!')
-  }
-})
+if (formContact !== null) {
+  formContact.addEventListener('submit', function(e) {
+    if (validateEmail(formemail.value) && formtheme.value !== '' && formtext.value !== '') {
+      
+    } else {
+      e.preventDefault();
+      alert('Ошибка ввода!')
+    }
+  })
+}
+
 
 // Функция для проверки email
 function validateEmail(email) {
@@ -19,20 +22,3 @@ function validateEmail(email) {
   return pattern.test(email);
 }
 
-// let colorbg = headerTitle.dataset.el;
-{
-  for (var i = 0; i < 100000; i++) {
-    setTimeout(function randColor() {
-      var headerTitle = document.querySelectorAll('.card');
-      headerTitle.forEach(function(item) {
-        var r = Math.floor(Math.random() * (256)),
-        g = Math.floor(Math.random() * (256)),
-        b = Math.floor(Math.random() * (256)),
-        background = '#' + r.toString(16) + g.toString(16) + b.toString(16);
-
-        item.style.borderColor = background;
-        item.querySelector('.card__img').style.borderBottom = `4px solid ${background}`;
-      })
-      }, 6000 * (i + 1));
-  }
-}
