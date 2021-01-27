1c7e09a388d691e3ba07935a5dbfb91937aa785a
Alex <sasha89253215539@yandex.ru>
2021-01-27 11:42:19 +0300

    test 01

--- docs/js/form.js
+++ docs/js/form.js
@@ -19,4 +19,20 @@ function validateEmail(email) {
   return pattern.test(email);
 }
 
+// let colorbg = headerTitle.dataset.el;
+{
+  for (var i = 0; i < 100000; i++) {
+    setTimeout(function randColor() {
+      var headerTitle = document.querySelectorAll('.card');
+      headerTitle.forEach(function(item) {
+        var r = Math.floor(Math.random() * (256)),
+        g = Math.floor(Math.random() * (256)),
+        b = Math.floor(Math.random() * (256)),
+        background = '#' + r.toString(16) + g.toString(16) + b.toString(16);
 
+        item.style.borderColor = background;
+        item.style.boxShadow =`'10px 14px 16px' + ${background}`;
+      })
+      }, 5000 * (i + 1));
+  }
+}
