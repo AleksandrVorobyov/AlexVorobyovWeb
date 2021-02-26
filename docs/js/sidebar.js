d6049db3deaea6b3a51a9c41b2d205e968837e4a
Alex <sasha89253215539@yandex.ru>
2021-02-26 16:30:44 +0300

    new bg + burger

--- docs/js/sidebar.js
+++ docs/js/sidebar.js
@@ -2,7 +2,8 @@ const burderBtn = document.getElementById('nav-burger-btn');
 const sidebar = document.getElementById('sidebar');
 var body = document.querySelector('body');
 
-burderBtn.addEventListener('click', function() {
+burderBtn.addEventListener('click', function(e) {
+    e.target.classList.toggle('navigation__burger--active');
     sidebar.classList.toggle('sidebar--active');
     body.classList.toggle('hidden');
 })
