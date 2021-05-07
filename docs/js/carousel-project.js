8d0eed5046c2cb0836b41dfcec444b415212b89c
Alex <sasha89253215539@yandex.ru>
2021-05-07 14:33:12 +0300

    fix bugs / delete full-page projects / new full-page for the header carousel

--- docs/js/carousel-project.js
+++ docs/js/carousel-project.js
@@ -9,37 +9,37 @@ $(document).ready(function(){
       mouseDrag: false,
       margin: 10
     });
-  });
+});
 
-  $(document).ready(function(){
-    $(".header__carousel").owlCarousel({
-      nav:false,
-      dots: false,
-      autoplay: true,
-      autoplayTimeout: 30000,
-      smartSpeed:2500,
-      loop: true,
-      margin: 20,
-      mouseDrag: false,
-      responsive: {
-        0 : {
-          items: 1
-        },
-        1024 : {
-          items: 2
-        },
-        1200 : {
-          items: 3
-        }
+$(document).ready(function(){
+  $(".header__carousel").owlCarousel({
+    nav:false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 30000,
+    smartSpeed:2500,
+    loop: false,
+    margin: 20,
+    mouseDrag: true,
+    responsive: {
+      0 : {
+        items: 1
+      },
+      1024 : {
+        items: 2
+      },
+      1200 : {
+        items: 3
       }
-      });
+    }
+    });
 
-      $('.header__carousel-btn-next').click(function() {
-        $(".header__carousel").trigger('next.owl.carousel', [2500]);
-    })
-    $('.header__carousel-btn-prev').click(function() {
-        $(".header__carousel").trigger('prev.owl.carousel', [2500]);
-    })
-  });
+    $('.header__carousel-btn-next').click(function() {
+      $(".header__carousel").trigger('next.owl.carousel', [2500]);
+  })
+  $('.header__carousel-btn-prev').click(function() {
+      $(".header__carousel").trigger('prev.owl.carousel', [2500]);
+  })
+});
 
 
--- docs/js/full-page.js
+++ docs/js/full-page.js
@@ -1,24 +1,19 @@
+const carouselHeader = document.querySelector('.header__carousel');
 const fullPage = document.querySelector('.full-page');
-const allCorouselImg = document.querySelectorAll('.carousel-project .owl-carousel-item');
 
-if (fullPage !== null) {
-    allCorouselImg.forEach(function(item) {
-        item.addEventListener('click', function() {
-            console.log("fire!");
-            body.style.overflow='hidden';
-            fullPage.classList.add('full-page--active');
-        })
-    })
-
-    const fullPageExit = document.querySelector('.full-page__exit');
-
-    fullPageExit.addEventListener('click', function () {
-        body.style.overflow='scroll';
-        fullPage.classList.remove('full-page--active');
-    })
-
-    fullPage.onclick = function(){
-        fullPage.classList.remove('full-page--active');
-        body.style.overflow='scroll';
+if (carouselHeader != null) {
+    let carouselHeaderItems = document.querySelectorAll('.owl-carousel-item');
+    for (let index = 0; index < carouselHeaderItems.length; index++) {
+        const element = carouselHeaderItems[index];
+        element.addEventListener('click', (e) => {
+            fullPage.classList.add('active');
+            document.querySelector('body').classList.add('hidden');
+            fullPage.querySelector('.full-page-item img').src = e.target.querySelector('img').src;
+        });
     };
+
+    fullPage.querySelector('.full-page-item').addEventListener('click', function() {
+        fullPage.classList.remove('active');
+        document.querySelector('body').classList.remove('hidden');
+    });
 }
